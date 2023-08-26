375cf929d9fd454f261b3421938df4ba87ea0c0a
Fatemeh <fatemehfaryadras7@gmail.com>
2023-08-26 16:07:43 +0330

    adding items to javascript

---
+++ assets/js/app.js
@@ -0,0 +1,63 @@
+const words = {
+    colors: [
+        "red",
+        "green",
+        "blue",
+        "yellow",
+        "orange",
+        "purple"
+    ],
+    cars: [
+        "mustang",
+        "corvette",
+        "camaro",
+        "challenger",
+        "charger",
+        "ferrari"
+    ],
+    fruits: [
+        "apple",
+        "blueberry",
+        "mandarin",
+        "pineapple",
+        "pomegranate",
+        "watermelon"
+    ],
+    animals: [
+        'Cat',
+        'Dog',
+        'Penguin',
+        'Lion',
+        'Horse',
+        'Elephant',
+        'Rabbit',
+        'Snake',
+        'Leopard',
+    ],
+    home: [
+        'Fork',
+        'Knife',
+        'Spoon',
+        'Chopst',
+        'Napkin',
+        'Glass',
+        'Plate',
+        'Bowl',
+        'Teaspoon',
+        'Mug'
+    ],
+    food: [
+        'Pizza',
+        'Burger',
+        'Pasta',
+        'Salad',
+        'Stew',
+        'Sushi',
+        'Cake',
+        'Kebab',
+        'Rice',
+        'Soup',
+    ],
+
+};
+
